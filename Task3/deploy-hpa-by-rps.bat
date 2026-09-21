f490cfcec6ee485bd2c5af2d6f149d04f112236f
Ivan Babikov <i.babikov@itransition.com>
2026-09-21 11:03:05 +0500

    Task3: добавление Prometeus

--- Task3/deploy-hpa-by-rps.bat
+++ Task3/deploy-hpa-by-rps.bat
@@ -2,8 +2,8 @@
 
 REM ============================================================
 REM  deploy-hpa-by-mem.bat
-REM  Deploy/redeploy scaletestapp into Minikube using hpa-by-rps.yaml Horizontal Pod Autoscaler (HPA) config.
+REM  Deploy/redeploy scaletestapp into Minikube using hpa-by-mem.yaml Horizontal Pod Autoscaler (HPA) config.
 REM ============================================================
 
-call "%~dp0deploy-common.bat" ./hpa-by-mem.yaml
+call "%~dp0deploy-common.bat" ./hpa-by-rps.yaml scaletestapp-hpa-rps
 exit /b %errorlevel%
