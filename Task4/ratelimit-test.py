9597d4897ea752973d06eb21f51ac4ce3f5127f2
Ivan Babikov <i.babikov@itransition.com>
2026-09-23 10:05:39 +0500

    Task4: тестирование rate-limiter - доработка настроек

--- Task4/ratelimit-test.py
+++ Task4/ratelimit-test.py
@@ -2,14 +2,18 @@ from locust import HttpUser, between, task
 from datetime import datetime, timezone
 
 class WebsiteUser(HttpUser):
-    wait_time = between(1, 5)
+    wait_time = between(0.1, 2)
 
     @task
     def get_web_api(self):
-        response = self.client.get("/api/web/", headers={"Client-Type": "web"}, name=" WEB API ")
+        response = self.client.get("/api/web/",
+                                   #headers={"Client-Type": "web"},
+                                   name=" WEB API ")
         print(f"{datetime.now(timezone.utc)} status_code: {response.status_code} body: {response.text}.")
 
     @task
     def get_mobile_api(self):
-        response = self.client.get("/api/mobile/", headers={"Client-Type": "mobile"}, name=" MOBILE API ")
+        response = self.client.get("/api/mobile/",
+                                   #headers={"Client-Type": "mobile"},
+                                   name=" MOBILE API ")
         print(f"{datetime.now(timezone.utc)} status_code: {response.status_code} body: {response.text}.")
